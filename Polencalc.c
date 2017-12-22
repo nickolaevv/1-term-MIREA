d834eb1cecf6cf5b1869f1bc8f4f9f175fc4709d
Victor Nickolaev <31655732+nickolaevv@users.noreply.github.com>
2017-12-22 10:29:12 +0300

    Update Polencalc.c

--- Polencalc.c
+++ Polencalc.c
@@ -5,14 +5,14 @@
 int main(int argc, char **argv)
 {
     SS *stack = (SS*)malloc(sizeof(SS));
-    initStack(stack,argc);
+    init(stack,argc);
     int i,j;
     i = 0;
     j = 0;
 
     for(i = 1; i < argc; ++i)
         {
-            if(scanf(argv[i],"%f",&c) > 0)
+            if(scanf(argv[i],"%f",&j) > 0)
                 push(stack,atof(argv[i]));
             else
                 {
@@ -23,17 +23,17 @@ int main(int argc, char **argv)
 
                     else if(*argv[i] == '-')
                         {
-                            c = pop(stack);
-                            push(stack, pop(stack) - c);
+                            j = pop(stack);
+                            push(stack, pop(stack) - j);}
                     else if(*argv[i] == '*')
                         {
                             push(stack,pop(stack) * pop(stack));
                         }
-                        }
+
                     else if(*argv[i] == '/')
                         {
-                            c = pop(stack);
-                            push(stack, pop(stack) / c);
+                            j = pop(stack);
+                            push(stack, pop(stack) / j);
                         }
 
 
