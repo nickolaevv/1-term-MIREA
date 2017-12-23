20a420982121499cca13b7fbcca41db10067d2cb
Victor Nickolaev <31655732+nickolaevv@users.noreply.github.com>
2017-12-24 02:46:36 +0300

    Update LS.c

--- LS.c
+++ LS.c
@@ -7,18 +7,21 @@
 int main (int argc,char** argv)
 {
     char file [SIZE];
-    DIR *directory = opendir(file);
-    struct dirent *cd = readdir(directory);
 
-    if (argc > 2)
+    if (argc >= 2)
         strcpy(file, argv[1]);
     else
         strcpy(file, ".");
 
     printf ("\n", file);
 
-    while (cd != NULL)
-        printf ("%s\n", cd -> d_name );
+    DIR *directory = opendir(file);
+    struct dirent *cd = readdir(directory);
+    
+    while ( (cd = readdir(directory)) != NULL) {
+        printf("%d - %s [%d] %d\n",
+               cd->d_ino, cd->d_name, cd->d_type, cd->d_reclen);
+    }
 
     closedir (directory);
     return 0;
